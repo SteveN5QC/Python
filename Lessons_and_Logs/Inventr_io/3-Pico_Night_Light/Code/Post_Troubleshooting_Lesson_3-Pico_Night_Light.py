e49cc880b79a18eab06c91f813661992fe16fc3b
Steve Nielson <n5qc@arrl.net>
2024-04-26 07:49:43 -0700

    Pushing code from MicroPython Lesson 3
     On branch RPi_20240422
     Changes to be committed:
    	new file:   Lessons_and_Logs/Inventr_io/3-Pico_Night_Light/Code/Lesson_3-Pico_Night_Light.py
    	deleted:    Lessons_and_Logs/Inventr_io/3-Pico_Night_Light/Code/Place_Holder.txt
    	new file:   Lessons_and_Logs/Inventr_io/3-Pico_Night_Light/Code/Post_Troubleshooting_Lesson_3-Pico_Night_Light.py
    	new file:   Lessons_and_Logs/Inventr_io/3-Pico_Night_Light/Code/REFERENCE_CODE_Lesson_3-Pico_Night_Light.py

---
+++ Lessons_and_Logs/Inventr_io/3-Pico_Night_Light/Code/Post_Troubleshooting_Lesson_3-Pico_Night_Light.py
@@ -0,0 +1,27 @@
+import machine
+import time
+
+# Define LED Pin
+LED_PIN = 17
+led = machine.Pin(LED_PIN, machine.Pin.OUT)
+
+# Define ADC Pin for LDR
+ADC_PIN = 26
+adc = machine.ADC(machine.Pin(ADC_PIN))
+
+# Define a threshold for the LDR reading below which the LED should turn ON
+LIGHT_THRESHOLD = 50000  # This might need adjustment based on your LDR and environment
+
+def night_light():
+    while True:
+        light_value = adc.read_u16()
+        print(light_value)
+        # If the reading is below the threshold, turn the LED on. Otherwise, turn it off.
+        if light_value > LIGHT_THRESHOLD:  #Was <
+            led.value(1)
+        else:
+            led.value(0)
+        
+        time.sleep(0.5)  # Check every half second
+
+night_light()
