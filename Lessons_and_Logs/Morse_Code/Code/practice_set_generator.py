aa90d7eeff2778da58752580eee398ab6c7289a0
Steve Nielson <N5QC@arrl.net>
2024-07-21 18:54:56 -0500

    Sunday 7/21/24 Updated Formatting of output
     On branch N5QC_Remote
     Changes to be committed:
    	modified:   Lessons_and_Logs/Morse_Code/Code/practice_set_generator.py
    	modified:   Lessons_and_Logs/Morse_Code/Notes/Morse_Practice_Sets_in_Python.docx

--- Lessons_and_Logs/Morse_Code/Code/practice_set_generator.py
+++ Lessons_and_Logs/Morse_Code/Code/practice_set_generator.py
@@ -1,3 +1,35 @@
+# Rev. 1 -- Update Output Formating -- 7/21/24
+
+
+import random
+
+def generate_groups(letters, num_groups):
+    letters = list(letters)
+    output = "vvv   "
+
+    for i in range(num_groups):
+        group = random.choices(letters, k=5)
+        formatted_group = ' '.join(group) + '  '
+        output += formatted_group
+        
+        if (i + 1) % 5 == 0:
+            output += " =  "
+    
+    output += "="
+    print(output)
+
+# Example usage:
+letters_input = input("Enter a string of letters: ")
+num_groups_input = int(input("Enter the number of groups: "))
+
+generate_groups(letters_input, num_groups_input)
+
+
+
+
+''' 
+WAS:  Rev. 0 -- Original -- 7/19/24
+
 import random
  
 def generate_groups(letters, num_groups):
@@ -14,3 +46,6 @@ letters_input = input("Enter a string of letters: ")
 num_groups_input = int(input("Enter the number of groups: "))
  
 generate_groups(letters_input, num_groups_input)
+
+
+'''
