cb5378472a9d47c7180dbadd5ac468f209450415
Steve <SteveN5QC@gmail.com>
2025-12-13 16:15:59 -0600

    Committing Dave Gray Lessons 2 and 3

---
+++ Lessons_and_Logs/DaveGrayCourse/Lesson_3/meaning.py
@@ -0,0 +1,13 @@
+meaning = 42
+print("")
+
+# print("The meaning of life is:", meaning)
+
+# if meaning > 10:
+#     print("Right on!")
+# else:
+#     print("Not today.")
+
+# Ternary operator example
+
+print("Right on!") if meaning > 10 else print("Not today.")
